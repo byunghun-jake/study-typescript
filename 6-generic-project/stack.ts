5b9385f32fda50362a46a7d45d3309addd53d03e
byunghun-jake <figma@kakao.com>
2021-09-11 22:44:32 +0900

    Generic: Generic Project

--- 6-generic-project/stack.ts
+++ 6-generic-project/stack.ts
@@ -1,38 +1,30 @@
 {
-  // Stack에 들어오는 Data의 Type 정의
-  type Data = string | number
-
-  // // 노드 클래스의 설계도 만들기
-  // interface Node {
-  //   readonly prev?: Node
-  //   readonly value: Data
-  // }
-
-  // class NodeImpl implements Node {
-  //   constructor(public value: Data, public prev?: Node) {}
-  // }
-  type StackNode = {
-    readonly value: Data
-    readonly next?: StackNode
-  }
+  /**
+   * 스택에 어떤 데이터든지 받을 수 있도록 제네릭을 사용해보자!
+   */
 
   // 스택 클래스의 설계도 만들기
-  interface Stack {
+  interface Stack<T> {
     readonly size: number
-    push(value: Data): void
-    pop(): Data
+    push: (value: T) => void
+    pop: () => T
   }
 
-  class StackImpl implements Stack {
+  type StackNode<T> = {
+    readonly value: T
+    readonly next?: StackNode<T>
+  }
+
+  class StackImpl<T> implements Stack<T> {
     // readonly를 구현하는 방법
     private _size: number = 0
     get size() {
       return this._size
     }
+    private head?: StackNode<T>
 
-    private head?: StackNode
-    push(value: Data) {
-      const node: StackNode = {
+    push(value: T) {
+      const node = {
         value,
         next: this.head,
       }
@@ -40,7 +32,7 @@
       this._size += 1
     }
 
-    pop(): Data {
+    pop(): T {
       // 의도적으로 얕은 null 체크를 하였다.
       if (this.head == null) {
         throw new Error("내부에 값이 없으요")
@@ -53,11 +45,19 @@
     }
   }
 
-  const stack = new StackImpl()
+  const stack = new StackImpl<string>()
   stack.push("하")
   stack.push("히")
   stack.push("호")
-  while (true) {
+  while (stack.size !== 0) {
     console.log(stack.pop())
+  }
+
+  const stack2 = new StackImpl<number>()
+  stack2.push(1)
+  stack2.push(2)
+  stack2.push(3)
+  while (stack2.size !== 0) {
+    console.log(stack2.pop())
   }
 }
